a99bbd898309cc30f9167698ba19c317285f7acc
Shahen <47934188+Shahen-M@users.noreply.github.com>
2019-02-24 02:17:12 -0800

    Rename index.php to sign_in.php

---
+++ sign_in.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <main>
+        <header>
+          <h1> Sign In </h1>
+        </header>
+
+        <section>
+          <p>Userame:</p>
+          <input type="text" name="username" placeholder="Username">
+
+          <p>Password:</p>
+          <input type="text" name="password" placeholder="Password">
+          <button type="button" name="submit"> Sign In </button>
+
+          <a href="sign_up.php">
+            <button type="button" name="submit"> Create Account </button>
+          </a>
+        </section>
+    </main>
+  </body>
+</html>
