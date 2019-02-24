98c8b273c79b92eadf9b72e89d389dcb1784143f
Shahen <47934188+Shahen-M@users.noreply.github.com>
2019-02-24 02:18:38 -0800

    Create sign_up.php

---
+++ sign_up.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <main>
+      <header>
+        <h1> Sign Up </h1>
+      </header>
+
+      <form action="account.php" method="post">
+        <p>Username:</p>
+        <input type="text" name="username" placeholder="Username">
+
+        <p>Password:</p>
+        <input type="text" name="password" placeholder="Password">
+
+        <p>E-mail:</p>
+        <input type="text" name="email" placeholder="Email">
+
+        <button type="submit" name="submit">Create Account</button>
+      </form>
+    </main>
+  </body>
+</html>
