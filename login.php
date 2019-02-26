86fba6f6c6f43b7363a75a191c82de3691f1c6ae
Shahen <47934188+Shahen-M@users.noreply.github.com>
2019-02-25 23:49:39 -0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,58 @@
+<?php
+
+if (isset($_POST['login-submit'])) {
+  require 'db.php';
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  if(empty($username) || empty($password)) {
+    header("Location /sign_in.php?error=emptyfields");
+    exit();
+
+  }
+  else {
+    $sql = "SELECT * FROM users WHERE firstName=? OR email=?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+      header("Location /sign_in.php?error=sqlerror");
+      exit();
+    }
+    else {
+
+      mysqli_stmt_bind_param($stmt, "ss", $username, $username);
+      mysqli_stmt_execute($stmt);
+      $result = mysqli_stmt_get_result($stmt);
+
+      if ($row = mysqli_fetch_assoc($result)) {
+        $pwdCheck = password_verify($password, $row['firstName']);
+        if ($pwdCheck == false) {
+          header("Location /sign_in.php?=wrongpassword");
+          exit();
+        }
+        else if ($pwdCheck == true) {
+          session_start();
+          $_SESSION['firstName'] = $row['firstName'];
+          $_SESSION['password'] = $row['password'];
+
+          header("Location /sign_in.php?=login=success");
+          exit();
+        }
+        else {
+          header("Location /sign_in.php?=wrongpassword");
+          exit();
+        }
+      }
+      else {
+        header("Location /sign_in.php?=nouser");
+        exit();
+      }
+
+    }
+  }
+
+}
+else {
+  header("Location /sign_in.php");
+  exit();
+}
