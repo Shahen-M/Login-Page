4ad358f43043238f2c7e85f5685d3ff028db2d82
Shahen <47934188+Shahen-M@users.noreply.github.com>
2019-03-01 23:36:19 -0800

    Update login.php

--- login.php
+++ login.php
@@ -1,58 +1,29 @@
 <?php
+  require "header.php";
+?>
 
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="hidden-content">
+      <header class="login-header">
+        <p>Login</p>
+      </header>
 
-if (isset($_POST['login-submit'])) {
-  require 'db.php';
+      <form action="includes/login.inc.php" method="post">
+        <input class="user-input" type="text" name="email" placeholder="Username/Email">
+        <input class="pass-input" type="password" name="password" placeholder="Password">
+        <button class="login-btn btn" type="submit" name="login-submit">Log In</button>
 
-  $mailuid = $_POST['email'];
-  $password = $_POST['password'];
+        <a href="signup.php">
+          <button class="signup-btn btn" type="button" name="signup-submit">Sign Up</button>
+        </a>
+      </form>
+    </div>
 
-  if(empty($mailuid) || empty($password)) {
-    header("Location: sign_in.php?error=emptyfields");
-    exit();
-  }
-  else {
-    $sql = "SELECT * FROM users WHERE firstName=? OR email=?;";
-    $stmt = mysqli_stmt_init($conn);
-    if (!mysqli_stmt_prepare($stmt, $sql)) {
-      header("Location: sign_in.php?error=sqlerror");
-      exit();
-    }
-    else {
-
-      mysqli_stmt_bind_param($stmt, "ss", $mailuid, $mailuid);
-      mysqli_stmt_execute($stmt);
-      $result = mysqli_stmt_get_result($stmt);
-
-      if ($row = mysqli_fetch_assoc($result)) {
-        $pwdCheck = password_verify($password, $row['password']);
-        if ($pwdCheck == false) {
-          header("Location: sign_in.php?=wr00ngpassword");
-          exit();
-        }
-        else if ($pwdCheck == true) {
-          session_start();
-          $_SESSION['userId'] = $row['firstName'];
-          $_SESSION['userUid'] = $row['password'];
-
-          header("Location: sign_in.php?=login=success");
-          exit();
-        }
-        else {
-          header("Location: sign_in.php?=wrongpassssssword");
-          exit();
-        }
-      }
-      else {
-        header("Location: sign_in.php?=nouser");
-        exit();
-      }
-
-    }
-  }
-
-}
-else {
-  header("Location /sign_in.php");
-  exit();
-}
+  </body>
+</html>
