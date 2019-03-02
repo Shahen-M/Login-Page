996cde52a9bfd3a727325a1b7f4e1d5708aa3a4f
Shahen <47934188+Shahen-M@users.noreply.github.com>
2019-03-01 23:38:14 -0800

    Update sign_in.php

--- sign_in.php
+++ sign_in.php
@@ -1,3 +1,7 @@
+<?php
+  session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -6,23 +10,27 @@
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
+
+    <header class="nav-header">
+      <nav>
+        <ul>
+          <li><a class="active" href="#home">Home</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </nav>
+
+      <a href="login.php">
+        <p id="sign-txt">Signin/Signout</p>
+      </a>
+    </header>
+
     <main>
-        <header>
-          <h1> Sign In </h1>
-        </header>
-
-        <section>
-          <p>Userame:</p>
-          <input type="text" name="username" placeholder="Username">
-
-          <p>Password:</p>
-          <input type="text" name="password" placeholder="Password">
-          <button type="button" name="submit"> Sign In </button>
-
-          <a href="sign_up.php">
-            <button type="button" name="submit"> Create Account </button>
-          </a>
-        </section>
+      <p> Login or create an account! </p>
     </main>
+
+    <script src="script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="crossorigin="anonymous"></script>
   </body>
 </html>
